e14522af5ae7d599cff6903a9ed08591d9ccb7b9
청주는사과아님 <jbw9964@gmail.com>
2024-11-07 18:27:52 +0900

    show properties
    (cherry picked from commit 084c96b756c00caaaf821458744a29c066c7678b)

--- src/main/java/core/testing/TestingApplication.java
+++ src/main/java/core/testing/TestingApplication.java
@@ -11,27 +11,19 @@ public class TestingApplication {
         ConfigurableApplicationContext context
                 = SpringApplication.run(TestingApplication.class, args);
 
-        printBeanName(context, AAA.class);
+        System.out.println("JPA interface : ");
+        AAA jpaRepo = context.getBean(AAA.class);
+        showProperties(jpaRepo);
 
-        try {
-            System.out.println(context.getBean("JPATestRepoImpl"));
-        }
-        catch (Exception e) {
-            System.out.println("no such bean");
-        }
-
-        try {
-            System.out.println(context.getBean("jPATestRepoImpl"));
-        }
-        catch (Exception e) {
-            System.out.println("no such bean");
-        }
+        System.out.println("\nImpl : ");
+        AAAImpl impl = context.getBean(AAAImpl.class);
+        showProperties(impl);
     }
 
-    private static void printBeanName(ConfigurableApplicationContext context, Class<?> clazz) {
-        for (String name : context.getBeanNamesForType(clazz)) {
-            System.out.println(name);
-        }
-        System.out.println();
+    private static void showProperties(Object o)    {
+        System.out.println("As string\t\t\t\t\t: " + o.toString());
+        System.out.println("Class\t\t\t\t\t\t\t: " + o.getClass());
+        System.out.println("Name\t\t\t\t\t\t\t: " + o.getClass().getSimpleName());
+        System.out.printf("IdentityHashcode\t: 0x%08x\n", System.identityHashCode(o));
     }
 }
